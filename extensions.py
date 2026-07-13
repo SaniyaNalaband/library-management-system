460d6a6a4e9ad3a94690a4a8f80af867fd9d6369
Saniya Nalaband <saniyanalaband642@gmail.com>
2026-07-13 14:16:47 +0530

    added Flask-Login setup

--- extensions.py
+++ extensions.py
@@ -4,5 +4,9 @@ Kept in its own file to avoid circular imports between app.py and models.
 """
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'  # redirects here if @login_required fails
+login_manager.login_message = 'Please log in to access this page.'
